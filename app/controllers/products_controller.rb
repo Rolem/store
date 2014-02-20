64c4f6f080a7abc898718548031216b718887a90
Andres F. Lopez <ing.ratosocial@gmail.com>
2014-02-20 11:09:43 -0500

    arreglo products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  include CurrentCart
+  before_action :set_cart
 
   # GET /products
   # GET /products.json
