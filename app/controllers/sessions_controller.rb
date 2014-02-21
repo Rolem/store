a5c1630692bdc8e92ee8bb1e062f4b36e5654e02
Andres F. Lopez <ing.ratosocial@gmail.com>
2014-02-21 01:41:30 -0500

    autenticacion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize 
   include CurrentCart
   before_action :set_cart, only: [:create]
   skip_before_action :authorize
