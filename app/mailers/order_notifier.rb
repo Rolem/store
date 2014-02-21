b9a944c808d621e32da9622ea8ecf53eb53403c4
Andres F. Lopez <ing.ratosocial@gmail.com>
2014-02-21 00:26:40 -0500

    Rolem store order confirmation

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "soporte@rolem.co"
+  default from: "ing.ratosocial@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,7 +9,7 @@ class OrderNotifier < ActionMailer::Base
   def received(order)
     @order = order
 
-    mail(to: order.email, subject: 'Pragmatic Store Order Confirmation')
+    mail(to: order.email, subject: 'Rolem Store Order Confirmation')
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -20,6 +20,6 @@ class OrderNotifier < ActionMailer::Base
   def shipped(order)
     @order = order
 
-    mail(to: order.email, subject: 'Pragmatic Store Order Shipped')
+    mail(to: order.email, subject: 'Rolem Store Order Shipped')
   end
 end
